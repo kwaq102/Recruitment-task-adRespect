de27b6ddca42214d6ec867295dd4099a7a5a3f79
Kamil Gajewski <kwaq102@gmail.com>
2023-08-22 11:46:37 +0200

    created gallery section

--- js/index.js
+++ js/index.js
@@ -4,3 +4,19 @@ const searchInput = document.querySelector("input.search");
 loupe.addEventListener("click", () => {
 	searchInput.classList.toggle("active");
 });
+
+// gallery
+
+const masonry = new Macy({
+	container: ".gallery",
+	mobileFirst: true,
+	columns: 1,
+	breakAt: {
+		768: 2,
+		992: 3,
+	},
+	margin: {
+		x: 30,
+		y: 30,
+	},
+});
